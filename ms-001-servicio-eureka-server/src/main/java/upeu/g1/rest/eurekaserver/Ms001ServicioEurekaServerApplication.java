a22e2a7014b83f275d1adf46c01656129d3edb0f
Zileri <yahairaarapa@upeu.edu.pe>
2021-07-12 11:33:56 -0500

    nuevo-eureka

--- ms-001-servicio-eureka-server/src/main/java/upeu/g1/rest/eurekaserver/Ms001ServicioEurekaServerApplication.java
+++ ms-001-servicio-eureka-server/src/main/java/upeu/g1/rest/eurekaserver/Ms001ServicioEurekaServerApplication.java
@@ -3,7 +3,6 @@ package upeu.g1.rest.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 @EnableEurekaServer
 @SpringBootApplication
 public class Ms001ServicioEurekaServerApplication {

eef2b383b131dac0f5ad6a4291fd04930541e6a8
yahairaarapa <yahairaarapa@upeu.edu.pe>
2021-07-12 01:20:17 -0500

    cambios2

--- ms-005-servicio-usuario/src/main/java/upeu/g1/rest/usuario/Ms005ServicioUsuarioApplication.java
+++ ms-005-servicio-usuario/src/main/java/upeu/g1/rest/usuario/Ms005ServicioUsuarioApplication.java
@@ -2,7 +2,9 @@ package upeu.g1.rest.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @EnableEurekaClient
 @SpringBootApplication
